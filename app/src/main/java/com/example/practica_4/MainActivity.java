04a90ecaa3a00fd18488972a1b9045e0ca61d6a3
miguelorg19 <139198305+miguelorg19@users.noreply.github.com>
2023-09-27 00:02:52 -0600

    Subida de proyecto inicial

---
+++ app/src/main/java/com/example/practica_4/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.practica_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = (Button)findViewById(R.id.btn1);
+        btn1.setOnClickListener(this);
+       }
+
+
+    @Override
+    public void onClick(View view) {
+        Intent i = new Intent(this, MainActivity2.class);
+        startActivity(i);
+
+    }
+}
+
